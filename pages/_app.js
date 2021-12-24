12f2c488f2a47524870fe2196f03393fed2fa08b
NGFuture <78330887+NGFuture@users.noreply.github.com>
2021-12-23 19:14:32 -0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import fire from '../config/fire-config';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Head>
+      <title>FixIt App </title>
+      <meta name='description' content='This app helps to keep track of orders'/>
+      <link rel="icon" href="/money_bag.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+  )
 }
 
-export default MyApp
+export default MyApp;
